e93ae9a59acc87cbaeb9899a9db3f683e3580597
Ariel David Mora Jarrín <arielabc389@gmail.com>
2025-05-31 18:32:40 -0500

    feat(repository): definir interfaces UsuarioRepository y TareaRepository (#9)

--- src/main/java/madstodolist/repository/TareaRepository.java
+++ src/main/java/madstodolist/repository/TareaRepository.java
@@ -4,4 +4,5 @@ import madstodolist.model.Tarea;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TareaRepository extends CrudRepository<Tarea, Long> {
+
 }
--- src/main/java/madstodolist/repository/UsuarioRepository.java
+++ src/main/java/madstodolist/repository/UsuarioRepository.java
@@ -2,9 +2,8 @@ package madstodolist.repository;
 
 import madstodolist.model.Usuario;
 import org.springframework.data.repository.CrudRepository;
-
 import java.util.Optional;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
-    Optional<Usuario> findByEmail(String s);
+    Optional<Usuario> findByEmail(String email);
 }
